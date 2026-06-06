a8a97b4637c239e4e7f7ce4c8e44c37033a2a0ef
Sanghmitra Shakya <sanghmitra9shakya@gmail.com>
2026-06-06 13:18:07 +0530

    Add MenuItemRepository - JPA repository layer

---
+++ src/main/java/com/example/foodapp/repository/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.foodapp.repository;
+
+import com.example.foodapp.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+    List<MenuItem> findByRestaurantId(Long restaurantId);
+}
